85d5bdb18423ee5fd43e79e97fbba29095efe4dd
Jishnu Mohan <jishnu7@gmail.com>
2012-10-24 20:35:52 +0530

    init!

---
+++ wp-playStore-widgets.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: PlayStore Widgets
+Plugin URI: http://blog.thecodecracker.com/
+Description: Wordpress PlayStore app details on wordpress blog.
+Version: 0.1.1
+Author: Jishnu Mohan
+Author URI: http://blog.thecodecracker.com
+*/
+
+
+?>
